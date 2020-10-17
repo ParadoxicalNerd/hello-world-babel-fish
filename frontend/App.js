e9cdf45980c897ae1d7524b0b8ed5bd5f3110d84
Pankaj <meghanipankaj@pme.me>
2020-10-18 01:53:54 +0530

    Added support for languages

--- frontend/App.js
+++ frontend/App.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import { Text, View, TextInput, Button } from 'react-native';
+import DropDownPicker from 'react-native-dropdown-picker';
+import Icon from 'react-native-vector-icons/Feather';
 
 const App = () => {
   let [input, setInput] = useState('Enter text to translate');
   let [translated, setTranslated] = useState('');
+  let [language, setLanguage] = useState('yoda')
 
   const translator = (val) => {
     return fetch('https://reactnative.dev/movies.json')
@@ -17,9 +20,25 @@ const App = () => {
         Input the text you want to translate
       </Text>
       <TextInput value={input} onChange={val => setInput(val)} />
+
       <Button title="Click to translate!" onPress={val => translator(val)} />
       <Text>{"Translated text"}</Text>
       <Text> {translated} </Text>
+      <DropDownPicker
+        items={[
+          { label: 'Yoda', value: 'yoda' },
+          { label: 'Sith', value: 'sith' },
+          { label: 'Gungan', value: 'gungan' }
+        ]}
+        defaultValue={language}
+        containerStyle={{ height: 40 }}
+        style={{ backgroundColor: '#fafafa' }}
+        itemStyle={{
+          justifyContent: 'flex-start'
+        }}
+        dropDownStyle={{ backgroundColor: '#fafafa' }}
+        onChangeItem={item => setLanguage(item)}
+      />
     </View>
   );
 }
